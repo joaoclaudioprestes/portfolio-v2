a390cc991c354d8aa779148af32e70f649ffe657
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-01-11 19:49:47 -0300

    :up:

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Menu } from "./components/Menu/index";
+
+function App() {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,13 @@
+
+export const Menu = () => {
+  return (
+    <div>
+        <div>
+            <img src="logo.svg" alt="Logo" />
+        </div>
+        <nav>
+          
+        </nav>
+    </div>
+  )
+}
