bd6d90f9ee901bcfe3359c14b3a526020be34a9c
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-01-12 13:31:56 -0300

    :up:

---
+++ src/components/Icon3D/index.jsx
@@ -0,0 +1,9 @@
+
+export const Icon3D = () => {
+  return (
+    <div style={ width: '400-x', height: '400px', background: '#723983' }>
+      
+    </div>
+  )
+}
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,30 @@ import "./_Home.sass";
 
 import { Header } from "../components/Header/index";
 import { GoArrowUpRight } from "react-icons/go";
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 export const Home = () => {
+  
   return (
     <>
-      <Header page="Home"/>
+      <Header page="Home" />
       <div className="container-home">
         <h2>Oi, eu sou João!</h2>
         <h1>Vamos transformar ideias em código?</h1>
         <h3>Full Stack Developer</h3>
         <p>
-        Desenvolvedor desde 2023 e estudante de Ciência da Computação. Em minha jornada acadêmica, busco constantemente a evolução na área Tech, explorando diariamente novas tecnologias e progredindo em meio a descobertas contínuas. Desde o início, aplico e registro meu aprendizado por meio de diversos projetos. Convido você a conferir um vislumbre do meu conhecimento em ação. 🚀
+          Desenvolvedor desde 2023 e estudante de Ciência da Computação. Em
+          minha jornada acadêmica, busco constantemente a evolução na área Tech,
+          explorando diariamente novas tecnologias e progredindo em meio a
+          descobertas contínuas. Desde o início, aplico e registro meu
+          aprendizado por meio de diversos projetos. Convido você a conferir um
+          vislumbre do meu conhecimento em ação. 🚀
         </p>
 
         <div className="box-buttons">
           <button>
             <Link to="/about" className="link">
-              Sobre mim <GoArrowUpRight />
+              Sobre mim <GoArrowUpRight className="icon" />
             </Link>
           </button>
         </div>
